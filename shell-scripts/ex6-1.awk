72c893594823e09b86d1a4de1ea84145a44e0de7
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-11-21 11:40:35 +0100

    Ex6-2

--- shell-scripts/ex6-1.awk
+++ shell-scripts/ex6-1.awk
@@ -1,3 +1,4 @@
+#!/usr/bin/awk -f
 BEGIN {FS=":"}      # Sets separator to :
 {printf "dn: uid=" $1 ", dc=example, dc=com\n"}
 {printf "cn: " $2 " " $3 "\n"}
--- shell-scripts/ex6-2.sh
+++ shell-scripts/ex6-2.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Prints top three users of disk space in /home
+
+TMPFILE="/tmp/tmp_ex6-2"
+
+# Stores information in format size:userid per file under /home
+find /home -type f -printf "%s:%U\n" > $TMPFILE
+
+# Iterates over previous result and sums up disk usage in format totalsize:userid
+# Sorts descending by totalsize and only prints top 3 
+# $2 = USERID, $1 = DISKUSAGE
+awk -F: '{diskusage[$2]+=$1} END{for (i in diskusage) printf "%s\t%s\n",diskusage[i],i}' $TMPFILE |
+sort -n -r | head -3 | numfmt --to=iec | awk 'BEGIN{printf "Size\t\tUserID\n"} {printf "%s\t\t%s\n",$1,$2}'
