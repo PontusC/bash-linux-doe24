dccb57bab3f26b9d1119d71b258af02b537471c9
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-11-21 20:02:00 +0100

    fixed regex

--- shell-scripts/ex5-7.sh
+++ shell-scripts/ex5-7.sh
@@ -6,4 +6,4 @@ FILENAME=$1                     # First parameter is file to check
 
 echo "prints all lines in \"$1\" containing trailing whitespace"
 echo "  line  match"
-cat -n $1 | sed -n -E '/.*\s+$/p'       # Matches 0 or more anything (.*) with 1 or more any whitespace (\s+) before end of line ($)
+cat -n $1 | sed -n -E '/\s+$/p'       # Matches 0 or more anything (.*) with 1 or more any whitespace (\s+) before end of line ($)
