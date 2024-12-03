160c3c457b53070b79a5d10ae4108b4b3d5bde30
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-12-03 10:27:38 +0100

    ex9

--- shell-scripts/whichdaemon-2.sh
+++ shell-scripts/whichdaemon-2.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-# SChecks if httpd and init daemons are running.
-# Prints status of services, and if httpd is running special message
 
-# Declare the two daemons/processes to find
-daemonOne="sshd"        # Special case daemon (httpd in exercise)
-daemonTwo="httpd"
+# Prints status of services
+
+# Presents the choices to user
+echo "Make a choice of daemon to check"
+cat << choices
+upower
+ssh
+docker 
+choices
+echo -n "Choice: "
+read choice
+
+# only char given as input
+if [[ "$choice" == [a-z] ]];then
+    echo "Only 1 char as input"
+    exit
+fi
 
 # Selects all processes (-e) and gets all starting with ? (means running without terminal)
 # Looks for matching process name at end of line
-runningDaemons=$(ps -eo 'tty,pid,comm' | grep ^? | grep -E "($daemonOne|$daemonTwo)$")
-# echo $runningDaemons
+runningDaemons=$(ps -eo 'tty,pid,comm' | grep ^? | grep -E $choice | awk '{printf "%s\t%s\n",$2,$3}')
 
 # Checks if given processes are found in previous search result 
-if [[ $(echo $runningDaemons | grep -E "$daemonOne$") ]];then
-    echo "$daemonOne is active"
-    echo "Webserver running";
+if [[ $(echo $runningDaemons | grep -E "$choice") ]];then
+    echo "$choice is active with below processes:"
+    echo "$runningDaemons"
 else
-    echo "$daemonOne is inactive";
-fi
-
-if [[ $(echo $runningDaemons | grep -E "$daemonTwo$") ]];then
-    echo "$daemonTwo is active";
-else
-    echo "$daemonTwo is inactive";
+    echo "$choice is inactive";
+    exit
 fi
